de70aac547e6b9b3d3321337b7ef07eb706de072
Yufan Zhang <brucezzzzzzz@outlook.com>
2022-03-22 15:51:44 +0800

    Update conv2d_ae.sh

--- autoencoder/conv2d_ae.sh
+++ autoencoder/conv2d_ae.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
 #SBATCH -N 1
-#SBATCH --job-name=conv2d_ae_1
-#SBATCH --ntasks=1
+#SBATCH -n 1
+#SBATCH -t 0-72:00:00
 #SBATCH -p gpu
+#SBATCH -q wildfire
+#SBATCH --job-name=conv2d_ae_1
 #SBATCH -o conv2d_ae_1.out
-#SBATCH --time=72:00:00
+#SBATCH -J con2d_ae_1
 
 source activate /home/hgao53/miniconda3/envs/af2_model
 
