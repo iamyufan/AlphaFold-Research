bac848a9aaa74bfb6baf4f4d53d398baf5ce431c
Yufan Zhang <brucezzzzzzz@outlook.com>
2022-03-22 15:15:24 +0800

    Update conv2d_ae.sh

--- autoencoder/conv2d_ae.sh
+++ autoencoder/conv2d_ae.sh
@@ -3,10 +3,9 @@
 #SBATCH -N 1
 #SBATCH --job-name=conv2d_ae_1
 #SBATCH --ntasks=1
-#SBATCH -p publicgpu
+#SBATCH -p gpu
 #SBATCH -o conv2d_ae_1.out
 #SBATCH --time=72:00:00
-#SBATCH --gres=gpu:1
 
 source activate /home/hgao53/miniconda3/envs/af2_model
 
