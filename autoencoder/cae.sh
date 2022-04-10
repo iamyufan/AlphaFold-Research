f4ded3ced0f8f188105de70916df2eae2e29f8d9
Yufan Zhang <brucezzzzzzz@outlook.com>
2022-04-10 10:06:18 +0800

    Update cae.sh

--- autoencoder/cae.sh
+++ autoencoder/cae.sh
@@ -1,16 +1,15 @@
 #!/bin/bash
 
 #SBATCH -N 1
-#SBATCH -n 1
-#SBATCH -c 100
+#SBATCH -n 8
 #SBATCH --job-name=ae_2d
-#SBATCH --output=ae_2d.out
-#SBATCH --error=ae_2d.err
+#SBATCH --output=ae_2d.%j.out
+#SBATCH --error=ae_2d.%j.err
 #SBATCH -t 0-72:00:00
 #SBATCH -p publicgpu
 #SBATCH -q wildfire
 #SBATCH --gres=gpu:1
-#SBATCH --cpus-per-task=16
+#SBATCH --cpus-per-task=10
 #SBATCH --mem=100G
 
 source /home/hgao53/miniconda3/bin/activate /home/hgao53/miniconda3/envs/af2_test
