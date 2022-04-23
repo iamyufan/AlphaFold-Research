4ba4011bd5da8aa500da0c77faced4e892b3a522
Yufan Zhang <brucezzzzzzz@outlook.com>
2022-04-23 22:05:26 +0800

    Update infer_ae.py

--- new_ae/infer_ae.py
+++ new_ae/infer_ae.py
@@ -18,44 +18,11 @@ def get_files(directory):
 
 def main():
     # Set device
-    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-
-    # Hyperparameters
-    # batch_size = args.batch_size
-    # num_epochs = args.num_epochs
-    # learning_rate = args.learning_rate
-
-    # # Load data
-    # root_dir = args.root_dir
-    # if args.dataset == '3d':
-    #     dataset = Dataset3D(
-    #         names_file = "filename3d.csv",
-    #         root_dir = root_dir,
-    #         transform = torch.from_numpy,
-    #     )
-    # elif args.dataset == '2d':
-    #     dataset = Dataset2D(
-    #         names_file = "filename2d.csv",
-    #         root_dir = root_dir,
-    #         transform = torch.from_numpy,
-    #     )
-
-    # dataloader = DataLoader(dataset=dataset, batch_size=batch_size, shuffle=True)
-
-    # Model
-    # if args.model == 'conv_ae_3d':
+    # device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    
     model = Conv3DAutoEncoder(in_channel=1).to('cpu')
-    # elif args.model == 'conv_ae_2d':
-    #     model = Conv2DAutoEncoder(in_channel=1).to('cpu')
-        
     PATH = 'conv_ae_3d_1.pth'
-    model.load_state_dict(PATH).double()
-
-    # Loss and optimizer
-    # criterion = nn.MSELoss()
-    # optimizer = optim.Adam(model.parameters(), lr=learning_rate)
-
-    # print(len(dataset))
+    model.load_state_dict(torch.load(PATH)).double()
     
     root_dir = "/scratch/hgao53/padded_bsu"
     SAVE_DIR = '/scratch/hgao53/encoded_bsu/'
@@ -86,60 +53,7 @@ def main():
         # Log
         print('Completed {}/{}, saved to {}'.format(i+1, len(file_names), SAVE_PATH))
         
-        
-        
-    # image = transform(Image.open(item).convert('L'))
-    # images = np.append(images, image.numpy())
-    
-
-    # print('================= Begin Inference =================')
-
-    # for data in enumerate(dataloader):
-    #     batch_time_start=time.time()
-        
-    #     # Get data to cuda if possible
-    #     i, repre = data
-    #     repre = repre.to('cpu').double()
-
-    #     # ==================forward==================
-    #     encoded, _ = model(repre)
-    #     encoded = encoded.squeeze(0).squeeze(0).squeeze(2)
-        
-        
-    #     # loss = criterion(bsu, decoded)
-
-    #     # ==================backward==================
-    #     # optimizer.zero_grad()
-    #     # loss.backward()
-    #     # optimizer.step()
-    #     # total_loss += loss.data
-        
-    #     batch_time_end=time.time()
-    
-    #     # ==================log==================
-    #     # print('=== epoch [{}/{}] -- batch [{}] -- time:{} -- loss:{:.4f}'
-    #     #     .format(epoch, num_epochs, i, batch_time_end-batch_time_start, total_loss))
-        
-    # # print('=== epoch [{}/{}] -- time:{}\n'.format(epoch, num_epochs, epoch_time_end-epoch_time_start))
-    # # torch.save(model.state_dict(), './conv_ae_3d_{0}.pth'.format(epoch))
 
 
 if __name__ == '__main__':
-    # import argparse
-
-    # parser = argparse.ArgumentParser(description = 'AutoEncoder')
-    # # Dataset
-    # parser.add_argument('-d', '--dataset', type=str, default='3d', help='Dataset')
-    # parser.add_argument('-dir', '--root-dir', type=str, default="/scratch/hgao53/padded_bsu",
-    #                     help='Dir for tensor-like data')
-    # parser.add_argument('-m', '--model', type=str, default='conv_ae_3d',)
-    
-    # # Training hyper-parameters
-    # parser.add_argument('-e', '--num-epochs', type=int, default=20, help='Number of epochs')
-    # parser.add_argument('-b', '--batch-size', type=int, default=1, help='Batch size')
-    # parser.add_argument('-lr', '--learning-rate', type=float, default=0.01, help='Learning rate')
-    # parser.add_argument('-p', '--path', type=str, default='conv_ae_3d_1.pth')
-    
-    # args = parser.parse_args().__dict__
-    # print(args)
     main()
